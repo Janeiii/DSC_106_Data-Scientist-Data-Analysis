1128b82ef947671a8d36441e8c5da05b3b67c4de
Janet Lin <linxiaoyingw@gmail.com>
2022-03-13 15:22:40 -0700

    Update

---
+++ app.js
@@ -0,0 +1,17 @@
+function finalproject(){
+    var filePath="data.csv";
+    question1(filePath);
+    /*
+    question2(filePath);
+    question3(filePath);
+    question4(filePath);
+    question5(filePath);
+
+     */
+}
+
+var question1=function(filePath){
+
+
+
+}

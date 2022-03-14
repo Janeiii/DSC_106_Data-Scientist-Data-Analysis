bb23e2ea30912fb8ed1e94705f8fc0ba8bdbfbdf
Janet Lin <linxiaoyingw@gmail.com>
2022-03-13 18:41:53 -0700

    update

--- app.js
+++ app.js
@@ -22,6 +22,22 @@ var question1=function(filePath){
     d3.csv(filePath, rowConverter).then(function (data) {
       console.log(data)
 
+        var svgheight = 600;
+        var svgwidth = 600;
+
+        var svg = d3.select("#q1_plot").append("svg")
+            .attr("height", svgheight)
+            .attr("width", svgwidth);
+
+        svg.selectAll("circle")
+            .data(data).enter().append("svg")
+            .attr("cx", function (d){
+                return d.
+            })
+
+
+
+
     })
 
 
